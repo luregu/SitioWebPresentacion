a26a257afe8c350711f2efba7635ff739f57363c
Luis Reyes <luregu@gmail.com>
2022-01-28 15:20:58 -0300

    Se sube sitio web con todas las secciones a excepcion de la seccion Contacto.

---
+++ ReadMe.txt
@@ -0,0 +1,6 @@
+Sitio Web Personal Responsivo
+
++ First Mobile
++ Uso de Flexbox 
++ Uso de  Grid
++ Uso de API Intersecion Observer 
--- js/app.js
+++ js/app.js
@@ -2,11 +2,48 @@ const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu");
 const closeMenuBtn = document.querySelector(".close-menu");
 
+
 function toggleMenu() {
     console.log('click', this)
     menu.classList.toggle("menu_opened");
 
 }
-console.log({ openMenuBtn });
+
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
+
+
+const menuLinks = document.querySelectorAll(".menu a[href^='#']");
+
+const observer = new IntersectionObserver((entries) => {
+    console.log("entries", entries);
+    entries.forEach(entry => {
+
+        const id = entry.target.getAttribute("id");
+        console.log("id ", id)
+        const menuLink = document.querySelector(`.menu a[href="#${id}"]`)
+        console.log("menuLink", menuLink);
+
+        if (entry.isIntersecting) {
+            document.querySelector(".menu a.selected").classList.remove("selected")
+            menuLink.classList.add("selected");
+        }
+    })
+}, { rootMargin: "-30% 0px -70% 0px" })
+
+
+menuLinks.forEach(menuLink => {
+    menuLink.addEventListener("click", function() {
+        menu.classList.remove("menu_opened");
+    })
+
+    const hash = menuLink.getAttribute("href");
+    console.log("hash", hash);
+    const target = document.querySelector(hash);
+    console.log("target", target);
+
+    if (target) {
+        observer.observe(target);
+    }
+
+});
